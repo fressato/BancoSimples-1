d517c6926c2627fef416e9221542fd92e8a1f7ab
fressato <101608384+fressato@users.noreply.github.com>
2024-06-03 10:35:21 -0300

    Add files via upload

---
+++ obj/ClienteGetAll.cs
@@ -0,0 +1,21 @@
+﻿using BancoSimples.Infra.Data;
+using BancoSimples.Domain.Conta;
+
+namespace BancoSimples.Endpoints.Cliente
+{
+    public class ClienteGetAll
+    {
+        public static string Template => "/cliente";
+        public static string[] Methods => new string[] {HttpMethod.Get.ToString()};
+        public static Delegate Handle => Action;
+
+        public static IResult Action( ApplicationDbContext context)
+        {
+            
+           var cliente = context.ClientePessoa.ToList();
+           //var response = cliente.Select(c => new ClienteResponse { Nome = c.Nome, Active = c.Active, Cpf = c.Cpf });
+
+            return Results.Ok(cliente);
+        }
+    }
+}
---
+++ obj/ClientePost.cs
@@ -0,0 +1,33 @@
+﻿using BancoSimples.Infra.Data;
+using BancoSimples.Domain.Conta;
+using Flunt.Notifications;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace BancoSimples.Endpoints.Cliente
+{
+    public class ClientePost
+    {
+        public static string Template => "/cliente";
+        public static string[] Methods => new string[] {HttpMethod.Post.ToString()};
+        public static Delegate Handle => Action;
+               
+        public static IResult Action(ClienteRequest clienteRequest, HttpContext http, ApplicationDbContext context)
+        {
+            var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var cliente = new ClientePessoa(clienteRequest.Nome, userId, userId);
+            
+            if (!cliente.IsValid)
+            {
+                return Results.ValidationProblem(cliente.Notifications.ConvertToProblemDetails());
+            }
+
+                
+
+            context.ClientePessoa.Add(cliente);
+            context.SaveChanges();
+
+            return Results.Created($"/cliente/{cliente.Id}", cliente.Id);
+        }
+    }
+}
